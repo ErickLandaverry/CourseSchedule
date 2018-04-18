c64e4f1a562cc287443c2775b14efe827f1feadd
landy <landy.erick20@gmail.com>
2018-04-18 15:56:58 -0600

    Modified these files

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
+import coursesReducer from './courses';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  
+  courses: coursesReducer
 });
 
 export default rootReducer;
